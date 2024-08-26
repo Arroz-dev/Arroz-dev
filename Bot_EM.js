e8783d99eeff933368a2b54c9fd5a3ed331a0e97
Arroz-dev <arrozpapu202123@gmail.com>
2024-08-25 18:14:18 -0600

    Bot_EM.js

--- Bot_EM.js
+++ Bot_EM.js
@@ -1,1 +1,29 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+
+const targetNumber = '+503 7182 3021';
+
+// Mensaje automático
+const autoReplyMessage = 'cerra el orto riko amanai murio';
+
+client.on('message', msg => {
+    if (msg.from === `${targetNumber}@c.us`) {
+        msg.reply(autoReplyMessage);
+    }
+});
+
+client.initialize();
 
